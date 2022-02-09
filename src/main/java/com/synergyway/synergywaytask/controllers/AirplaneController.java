f35bc4b9494a78d954db0c3761dc5776781b8932
burakozcelik5028 <burakozcelik5028@hotmail.com>
2022-02-10 02:56:48 +0300

    AirplaneController

--- src/main/java/com/synergyway/synergywaytask/controllers/AirplaneController.java
+++ src/main/java/com/synergyway/synergywaytask/controllers/AirplaneController.java
@@ -1,8 +1,8 @@
 package com.synergyway.synergywaytask.controllers;
 
+import com.synergyway.synergywaytask.entities.Airplane;
 import com.synergyway.synergywaytask.services.AirplaneServices.AirplaneService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/airplanes")
@@ -12,5 +12,25 @@ public class AirplaneController {
 
     public AirplaneController(AirplaneService airplaneService) {
         this.airplaneService = airplaneService;
+    }
+
+    @PostMapping("/save")
+    public Airplane saveAirPlane(@RequestBody Airplane airplane){
+        return airplaneService.saveAirPlane(airplane);
+    }
+
+    @PutMapping("/update")
+    public Airplane updateAirPlane(@RequestBody Airplane airplane){
+        return airplaneService.updateAirPlane(airplane);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deleteAirPlane(@PathVariable("id") Long airplaneID){
+        return airplaneService.deleteAirPlane(airplaneID);
+    }
+
+    @PutMapping("/updateaircompany/{airplaneID}/{aircompanyID}")
+    public boolean updateAirCompanyID(@PathVariable Long airplaneID, @PathVariable Long airCompanyID){
+        return airplaneService.updateAirCompanyID(airplaneID, airCompanyID);
     }
 }
