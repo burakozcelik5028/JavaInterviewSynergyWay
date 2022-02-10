da81a6e55a9a665643dfd439c1e09ddad8406678
burakozcelik5028 <burakozcelik5028@hotmail.com>
2022-02-10 03:02:58 +0300

    Flight controller

--- src/main/java/com/synergyway/synergywaytask/controllers/FlightController.java
+++ src/main/java/com/synergyway/synergywaytask/controllers/FlightController.java
@@ -1,8 +1,11 @@
 package com.synergyway.synergywaytask.controllers;
 
+import com.synergyway.synergywaytask.entities.Flight;
+import com.synergyway.synergywaytask.enums.FlightStatus;
 import com.synergyway.synergywaytask.services.FlightServices.FlightService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/flights")
@@ -12,5 +15,25 @@ public class FlightController {
 
     public FlightController(FlightService flightService) {
         this.flightService = flightService;
+    }
+
+    @PostMapping("/save")
+    public Flight createFlight(@RequestBody Flight flight){
+        return flightService.createFlight(flight);
+    }
+
+    @PutMapping("/update")
+    public Flight updateFlight(@RequestBody Flight flight){
+        return flightService.updateFlight(flight);
+    }
+
+    @GetMapping("/getbystatus/{status}")
+    public ArrayList<Flight> getFlightsByStatus(@PathVariable FlightStatus status){
+        return flightService.getFlightsByStatus(status);
+    }
+
+    @GetMapping("/getspecial")
+    public ArrayList<Flight> getAllFlightsActiveStatusAndStartedMoreThan24HoursAgo(){
+        return flightService.getAllFlightsActiveStatusAndStartedMoreThan24HoursAgo();
     }
 }
